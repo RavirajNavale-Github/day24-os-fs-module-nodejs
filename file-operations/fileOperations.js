a5d8eda88ce768174aec15a9eaf1b7aa60ce6f55
RavirajNavale-Github <165894463+RavirajNavale-Github@users.noreply.github.com>
2024-05-07 18:24:57 +0530

    methods added

--- file-operations/fileOperations.js
+++ file-operations/fileOperations.js
@@ -4,39 +4,58 @@ const fs = require("fs");
 const filePath = "file.txt";
 
 //create file
-fs.writeFile(filePath, "I am File", (err) => {
-    if(err != null){
-        console.log("err: ", err);
-    }else{
-        console.log(`File ${filePath} Create Succeddfully.`)
-    }
-});
+// fs.writeFile(filePath, "I am File", (err) => {
+//     if(err != null){
+//         console.log("err: ", err);
+//     }else{
+//         console.log(`File ${filePath} Create Succeddfully.`)
+//     }
+// });
+
+console.log('----------------------------------------------------------------------------')
 
 // Read file content
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
-  console.log("File Content:", data);
-});
+// fs.readFile(filePath, "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error reading file:", err);
+//     return;
+//   }
+//   console.log("File Content:", data);
+// });
+
+console.log('----------------------------------------------------------------------------')
 
 // Append text to file
-const newText = "\nNew line appended.";
-fs.appendFile(filePath, newText, "utf8", (err) => {
-  if (err) {
-    console.error("Error appending text to file:", err);
-    return;
-  }
-  console.log("Text appended to file successfully.");
-});
+// const newText = "\nNew line appended.";
+// fs.appendFile(filePath, newText, "utf8", (err) => {
+//   if (err) {
+//     console.error("Error appending text to file:", err);
+//     return;
+//   }
+//   console.log("Text appended to file successfully.");
+// });
 
-// Update file content
+console.log('----------------------------------------------------------------------------')
+
+// Update/Create file content
 const updatedContent = "Updated file";
 fs.writeFile(filePath, updatedContent, "utf8", (err) => {
   if (err) {
     console.error("Error updating file content:", err);
     return;
   }
-  console.log("File content updated successfully.");
+  console.log("File content updated successfully."); 
 });
+
+console.log('----------------------------------------------------------------------------')
+
+// Delete File
+// fs.unlink(filePath, (err) => {
+//     if(err){
+//         console.log('Error: ', err);
+//     }else{
+//         console.log("File Deleted Successfully.")
+//     }
+// });
+
+console.log('----------------------------------------------------------------------------')
--- node-os-operations/osOperations.js
+++ node-os-operations/osOperations.js
@@ -5,14 +5,20 @@ const totalMemory = os.totalmem();
 console.log(`Total Memory: ${totalMemory}`);
 // console.log(`Total Memory: ${((totalMemory)/(1024*1024*1024))}`);
 
+console.log('----------------------------------------------------------------------------')
+
 //Get free memory (in bytes)
 const freeMemory = os.freemem();
 console.log(`Free Memory: ${freeMemory}`);
 // console.log(`Free Memory: ${(freeMemory)/(1024*1024*1024)}`)
 
+console.log('----------------------------------------------------------------------------')
+
 // Get OS platform
 const platform = os.platform();
 console.log(`OS Plateform: ${platform}`);
+
+console.log('----------------------------------------------------------------------------')
 
 // Get CPU information
 const cpus = os.cpus();
@@ -21,17 +27,25 @@ cpus.forEach((cpu, index) => {
   console.log(`CPU ${index + 1} : ${cpu.model}`);
 });
 
+console.log('----------------------------------------------------------------------------')
+
 // Get current user info
 const userInfo = os.userInfo();
 console.log(`Current user info: ${JSON.stringify(userInfo)}`);
 
+console.log('----------------------------------------------------------------------------')
+
 //Network Interface
 const networkInterfaces = os.networkInterfaces;
-console.log(networkInterfaces.length)
+console.log('Network Interfaces Length: ',networkInterfaces.length)
+
+console.log('----------------------------------------------------------------------------')
 
 //uptime
 const uptime = os.uptime();
 console.log(`System uptime: ${uptime}`);
+
+console.log('----------------------------------------------------------------------------')
 
 // Helper function to format bytes
 // function formatBytes(bytes) {
@@ -44,3 +58,5 @@ console.log(`System uptime: ${uptime}`);
 //   }
 //   return `${size.toFixed(2)} ${units[unitIndex]}`;
 // }
+
+console.log('----------------------------------------------------------------------------')
